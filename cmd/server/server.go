6f0c50d781f19b7c65caa535d3f9c04c9bb93653
Renata <renata.francisborges@gmail.com>
2024-04-16 20:14:12 -0300

    GraphQL with go

---
+++ cmd/server/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/renatafborges/GraphQL-with-go/graph"
+	"github.com/renatafborges/GraphQL-with-go/internal/database"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", (err))
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	courseDB := database.NewCourse(db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+		CourseDB:   courseDB,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
